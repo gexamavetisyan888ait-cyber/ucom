23f9d04abd79430886d835ef9ea83b411419939e
Gexam Avetisyan <gexam.avetisyan888ait@gmail.com>
2026-01-17 16:14:36 +0400

    Add Footer in Rouming page

--- src/pages/Rouming.jsx
+++ src/pages/Rouming.jsx
@@ -3,6 +3,7 @@ import Header from '../Components/Header/Header'
 import RoumingFirst from '../Components/RoumingFirst/RoumingFirst'
 import UcomRouming from '../Components/UcomRouming/UcomRouming'
 import RoamingPage from '../Components/RoumingPage/RoamingPage'
+import Footer from "../Components/Footer/Footer"
 
 export default function Rouming() {
   return (
@@ -11,6 +12,7 @@ export default function Rouming() {
         <RoumingFirst/>
         <UcomRouming/>
         <RoamingPage/>
+        <Footer/>
     </div>
   )
 }
