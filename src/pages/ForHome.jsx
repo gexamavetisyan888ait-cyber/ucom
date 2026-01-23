da300371c932b0c50a3c99053aefdfb18fa6317e
Gexam Avetisyan <gexam.avetisyan888ait@gmail.com>
2026-01-23 17:04:36 +0400

    add Footer in ForHome

--- src/pages/ForHome.jsx
+++ src/pages/ForHome.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Components/Header/Header";
 import ForHomeCards from '../Components/ForHomeCards/ForHomeCards'
 import InternetInfo from '../Components/InternetInfo/InternetInfo'
+import Footer from "../Components/Footer/Footer";
 
 export default function ForHome() {
   return (
@@ -9,6 +10,7 @@ export default function ForHome() {
       <Header />
       <ForHomeCards />
       <InternetInfo/>
+      <Footer/>
     </div>
   );
 }
